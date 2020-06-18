e4beb3a698e0a3c3fe5415a737f9f8fe4a3b2b4c
Oswaldo Gonzalez <therkash22@gmail.com>
2020-06-18 08:49:51 -0500

    listo para despliegue

--- verificar.js
+++ verificar.js
@@ -7,7 +7,8 @@ async function enviarFormulario(ev) {
     enviarFormulario.enviando = true;
     var form = document.querySelector("form");
     var result = document.querySelector(".form-msg");
-    result.innerHTML = "Enviando, por favor espera...";
+    result.innerHTML = '<div class="p-3 mb-2 bg-info text-white">Enviando los datos a verficar, por favor espera...</div>';
+    //'<div class="p-3 mb-2 bg-info text-white">Enviando, por favor espera...</div>'
 
     var datos = new FormData(form);
     var init = {
@@ -31,7 +32,38 @@ async function enviarFormulario(ev) {
     // permitimos volver a enviar el formulario de nuevo
     enviarFormulario.enviando = false;
 }
+async function enviarFormularioNIT(ev) {
+    ev.preventDefault();
+    if (enviarFormulario.enviando) { return; }
+    enviarFormulario.enviando = true;
+    var form = document.querySelector("#nitForm");
+    var result = document.querySelector(".form-msg-nit");
+    result.innerHTML = '<div class="p-3 mb-2 bg-info text-white">Enviando los datos a verficar, por favor espera...</div>';
+
+    var datos = new FormData(form);
+    var init = {
+        method: form.method,
+        body: datos
+    };
+    // petición ajax con fetch
+    try {
+        var response = await fetch(form.action, init);
+        if (response.ok) {
+            var respuesta = await response.json();
+            console.log(respuesta);
+            result.innerHTML = '<div class="p-3 mb-2 bg-success text-white">La empresa Esta Autorizado para laborar</div>';
+            form.reset();
+        } else {
+            throw new Error(response.statusText);
+        }
+    } catch (err) {
+        result.innerHTML = '<div class="p-3 mb-2 bg-danger text-white">La empresa NO esta Autorizada</div>';
+    }
+    // permitimos volver a enviar el formulario de nuevo
+    enviarFormulario.enviando = false;
+}
 
 document.addEventListener("DOMContentLoaded", function() {
-    document.querySelector("form").addEventListener("submit", enviarFormulario);
+    document.querySelector("#cedulaForm").addEventListener("submit", enviarFormulario);
+    document.querySelector("#nitForm").addEventListener("submit", enviarFormularioNIT);
 });
